3c9366274f4d738d331ef5c68dd3404395a3bf43
Marquin <marcus1224santos@hotmail.com.br>
2025-04-06 23:46:04 -0300

    change httprequest

--- httprequest/assets/js/main.js
+++ httprequest/assets/js/main.js
@@ -1,97 +1,53 @@
-class ValidaFormulario {
-    constructor(){
-        this.formulario = document.querySelector('.formulario'); 
-        this.eventos();
-    }
+const request = (obj) => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(obj.method, obj.url, true);
+    xhr.send();
 
-    eventos(){
-        this.formulario.addEventListener('submit', e => {
-            this.handleSubmit(e);
-        })
-    }
+    xhr.addEventListener("load", () => {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.responseText);
+      } else {
+        reject({
+          code: xhr.status,
+          msg: xhr.statusText,
+        });
+      }
+    });
+  });
+};
 
-    handleSubmit(e) {
-        e.preventDefault();
-        const camposValidos = this.camposSaoValidos();
-        const senhasValidas = this.senhasSaoValidas();
+document.addEventListener("click", (e) => {
+  const el = e.target;
+  const tag = el.tagName.toLowerCase();
 
-        if(camposValidos && senhasValidas){
-            alert("Formulário Enviado");
-            this.formulario.submit();
-        }
-    }
+  if (tag === "a") {
+    e.preventDefault();
+    carregaPagina(el);
+  }
+});
 
-    senhasSaoValidas() {
-        let valid = true;
-        const senha = this.formulario.querySelector('.senha');
-        const repetirSenha = this.formulario.querySelector('.repetir-senha');
-        if(senha.value !== repetirSenha.value){
-            valid = false;
-            this.criaErro(senha, "Campos senha e repetir senha precisam ser iguais")
-            this.criaErro(repetirSenha, "Campos senha e repetir senha precisam ser iguais")
-        }
-        if(senha.value.length < 6 || senha.value.length > 12){
-            valid = false
-            this.criaErro(senha, "Senha precisa estar entre 6 e 12 caracteres")
-        }
-        return valid
-    }
-
-    camposSaoValidos(){
-        let valid = true;
-        for(let erroText of this.formulario.querySelectorAll('.error-text')){
-            erroText.remove()
-        }
-
-        for(let campo of this.formulario.querySelectorAll('.validar')){
-            const label = campo.previousElementSibling.innerText;
-            if(!campo.value){
-                this.criaErro(campo, `${label} não pode estar em branco`)
-                valid = false;
-            }
-
-            if(campo.classList.contains('cpf')){
-                if(!this.validaCPF(campo)) valid = false;
-            }
-
-            if(campo.classList.contains('usuario')){
-                if(!this.validaUsuario(campo)) valid = false;
-            }
-        }
-
-        return valid;
-    }
-
-    validaUsuario(campo){
-        const usuario = campo.value;
-        let valid = true
-        if (usuario.length < 3 || usuario.length > 12) {
-            this.criaErro(campo, 'Usuario precisa ter entre 3 e 12 caracters.');
-            return false;
-        }
-        if (!usuario.match(/[a-zA-Z0-9+$]/g)) {
-            this.criaErro(campo, 'Nome de usuário precisa conter apenas letras ou numeros.');
-            return false;
-        }
-        return valid;
-    }
-
-    validaCPF(campo) {
-        const cpf = new ValidaCPF(campo.value);
-        if (!cpf.checkValid()) {
-            this.criaErro(campo, 'CPF inválido.');
-            return false;
-        }
-        return true;
-    }
-    
-
-    criaErro(campo, msg) {
-        const div = document.createElement('div');
-        div.innerHTML = msg;
-        div.classList.add('error-text');
-        campo.insertAdjacentElement('afterend', div);
-    }
+async function carregaPagina(el) {
+  try {
+    const href = el.getAttribute("href");
+    const response = await fetch(href);
+    if (response.status !== 200) throw new Error("ERRO 404!");
+    const html = await response.text();
+    carregaResultado(html);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-const valida = new ValidaFormulario();
+function carregaResultado(response) {
+  const resultado = document.querySelector(".resultado");
+  resultado.innerHTML = response;
+}
+
+fetch("pagina4.html")
+  .then((resposta) => {
+    if (resposta.status !== 200) throw new Error("ERRO 404 NOSSO");
+    return resposta.text();
+  })
+  .then((html) => console.log(html))
+  .catch((e) => console.log(e));
